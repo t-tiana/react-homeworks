dfbde3d6effa7d1de23c7e5d43a92ddd542f6628
T.Tiana <t.tiana@keemail.me>
2021-04-17 01:20:03 +0300

    restore 18_homework_02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
 import s from './App.module.css';
-import TodoApp from './components/TodoApp';
+
+// 1. Створити класовий компонент в якому з допомогою стану компоненту всі дані з інпута
+//в який юзер буде вводити тест - будуть в режимі реального часу відображатись в функціональному компоненті в h1 тегу.
+import Input from './Components/InputToOutput';
+
+// 2. Створити 3 компонента крізь які будемо передавати функцію з рандомним числом та
+// повертати в головний компонент
+// де отримане значення з компонента С будемо множити на 1000 та записувати в стейт . (А -> B -> C > B > A)
+import RandomGen from './Components/RandomGen';
 
 const App = () => {
   return (
     <div className={s.wrapper}>
-      <TodoApp />
+      <Input />
+      <RandomGen />
     </div>
   );
 };
---
+++ src/components/RandomGen/AComponent/AComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './AComponent.module.css';
+import BComponent from './../BComponent';
+
+class AComponent extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  setAState = evt => {
+    this.setState(() => ({
+      count: this.state.count + evt,
+    }));
+    console.log(this.state.count);
+  };
+
+  render() {
+    return (
+      <div className={s.wrapper}>
+        <BComponent setAState={this.setAState} stateBCount={this.state.count} />
+        <h1>{(this.state.count * 1000).toFixed(0)}</h1>
+      </div>
+    );
+  }
+}
+
+export default AComponent;
